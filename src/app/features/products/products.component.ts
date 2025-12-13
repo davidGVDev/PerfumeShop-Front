2c3445504cda13999116cf2f9295b8a492059edb
David <david38ing@gmail.com>
2025-12-13 14:42:07 -0600

    filters style

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
-import { ProductCardHorizontal } from '../../shared/components/product-card-horizontal/product-card-horizontal';
-import { ProductCardVertical } from '../../shared/components/product-card-vertical/product-card-vertical';
+import { Component, signal } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+export interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-products',
-  imports: [ProductCardVertical, ProductCardHorizontal],
+  imports: [
+    // ProductCardVertical,
+    // ProductCardHorizontal,
+    MatAutocompleteModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+    MatIconModule,
+    MatExpansionModule,
+    MatButtonModule
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+
+  myControl = new FormControl<string | User>('');
+  options: User[] = [{name: 'Mary'}, {name: 'Shelley'}, {name: 'Igor'}];
+  filteredOptions: Observable<User[]> = new Observable<User[]>();
+  readonly panelOpenState = signal(false);
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
 
 }
